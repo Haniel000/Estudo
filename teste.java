83cc9dea994938f3d452a37cb767916fc8f3091c
Lipe <filipecouto33@gmail.com>
2025-03-12 09:33:20 -0300

    Add files via upload

---
+++ teste.java
@@ -0,0 +1,67 @@
+// Hello World 
+// public class teste {
+//     public static void main(String[] args){
+//         System.out.println("Hello World");
+//     }
+// }
+
+
+// Scanner de Idade + Nome
+// import java.util.Scanner;
+
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.println("Bota o nome aí");
+//         String name = scanner.nextLine();
+
+//         System.out.println("Idade: ");
+//         int age = scanner.nextInt();
+
+//         System.out.println("Eai " + name);
+//         System.out.println("Velhão man, " + age + " anos dog?");
+
+//         scanner.close();
+//     }
+// }
+
+
+// If Else Elif o caralho é else if
+
+// public class Main{
+//     public static void main(String[] args){
+//         int number = 10;
+
+//         if (number > 20) {
+//             System.out.println("Maior que 20");
+//         }else if (number >10){
+//                 System.out.println("Número menor que 20 = 10");
+//         } else {
+//             System.out.println("Número é 10 ou menos");
+//         }
+//     }
+// }
+
+
+// Scanner de número
+
+// import java.util.Scanner;
+
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("Escolhe um número aí: ");
+//         int numero  = scanner.nextInt();
+
+//         if (numero>=0 && numero<=29) {
+//             System.out.println("Número maior ou igual a 10");
+//         }else if (numero>=30 && numero<=59) {
+//             System.out.println("Número 30 ou maior ");
+//         }else{
+//             System.out.println("vai pra narnia é filho da puta ");
+//         }
+
+//         scanner.close();
+//     }
+// }
